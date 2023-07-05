19deecd1d64c8a04f0df9b284cdee4b7dc293a9a
Cesc <83516572+cescps@users.noreply.github.com>
2023-07-05 14:10:40 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const playerBoxes = document.querySelectorAll('.player-box');
+const audioElements = [];
 
 playerBoxes.forEach(function(playerBox) {
+  const audio = new Audio();
+  audioElements.push(audio);
+
   const triangleIcon = playerBox.querySelector('.triangle-icon');
   const pauseIcon = playerBox.querySelector('.pause-icon');
   let isPlaying = false;
 
   const songUrl = playerBox.dataset.songUrl;
-  const audio = new Audio(songUrl);
+  audio.src = songUrl;
 
   playerBox.addEventListener('click', function() {
     if (isPlaying) {
@@ -22,3 +26,19 @@ playerBoxes.forEach(function(playerBox) {
     isPlaying = !isPlaying;
   });
 });
+
+$("#volume").slider({
+  min: 0,
+  max: 100,
+  value: 0,
+  range: "min",
+  slide: function(event, ui) {
+    setVolume(ui.value / 100);
+  }
+});
+
+function setVolume(volume) {
+  audioElements.forEach(function(audio) {
+    audio.volume = volume;
+  });
+}
